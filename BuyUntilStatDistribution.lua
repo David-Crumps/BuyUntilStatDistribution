d85e6cb586178b25efe32ccb40667b6ab18b0ff3
David-Crumps <david.crumplin@gmail.com>
2025-12-12 18:57:21 +0200

    Refactored, implemented sliders in the brunts armoury, that you can use to select the stat values you want

--- BuyUntilStatDistribution.lua
+++ BuyUntilStatDistribution.lua
@@ -3,19 +3,27 @@ local ItemUtils = require("scripts/utilities/items")
 local MasterItems = require("scripts/backend/master_items")
 local WeaponStats = require("scripts/utilities/weapon_stats")
 
-local MasterData = require("scripts/backend/master_data")
-local CraftingUtil = require("scripts/backend/crafting")
-local WeaponTempalte = require("scripts/utilities/weapon/weapon_template")
+--local WeaponTemplate = require("scripts/utilities/weapon/weapon_template")
+local WeaponTemplates = require("scripts/settings/equipment/weapon_templates/weapon_templates")
 
+local UIRenderer = require("scripts/managers/ui/ui_renderer")
+local UIWidget = require("scripts/managers/ui/ui_widget")
+local UISoundEvents = require("scripts/settings/ui/ui_sound_events")
+
+local TextInputPassTemplates = require("scripts/ui/pass_templates/text_input_pass_templates")
+local SliderPassTemplates = require("scripts/ui/pass_templates/slider_pass_templates")
+local UIFontSettings = require("scripts/managers/ui/ui_font_settings")
 
 local STAT_THRESHOLD = 380
+local NUMBER_OF_STATS = 5
+local views = "scripts/ui/views/"
 
-mod._stat_1 = 0
-mod._stat_2 = 0
-mod._stat_3 = 0
-mod._stat_4 = 0
-mod._stat_5 = 0
+
 mod._just_purchased = false
+mod._user_stats = {}
+
+mod._selected_weapon = ""
+
 
 mod.stats_exceed = false
 mod.INFO_MSG = "INFO: Stats fall within acceptable range"
@@ -47,10 +55,14 @@ local function _item_property_list(item)
 end
 
 local function total_stats()
-    return mod._stat_1 + mod._stat_2 + mod._stat_3 + mod._stat_4 + mod._stat_5
+    local total = 0
+    for stat, data in pairs(mod._user_stats) do
+        total = total + data.value
+    end
+    return total
 end
-
-
+--THIS STAT_CHECK FUNCTION NEEDS TO BE UPDATED
+--[[
 local stat_check = function(force_startup)
     local total = total_stats()
     if (total > STAT_THRESHOLD) then
@@ -63,19 +75,14 @@ local stat_check = function(force_startup)
         mod.stats_exceed = false
     end
 end
-
+--]]
 local _init = function()
-    mod._stat_1 = mod:get("stat_1")
-    mod._stat_2 = mod:get("stat_2")
-    mod._stat_3 = mod:get("stat_3")
-    mod._stat_4 = mod:get("stat_4")
-    mod._stat_5 = mod:get("stat_5")
+    mod._user_stats = {}
 end
 
-function mod.on_setting_changed(setting_id)
-    _init()
-    stat_check(false)
-end
+-- to insert a new widget look at stuff searcher it does it well
+-- use hook_require -> so that when the file is called it will automatically run a function we create that will add ->
+-- atleast text input streams if not bars hopefully to the right of the screen along with the stat names as well.
 
 mod:hook_safe("CreditsGoodsVendorView", "_on_purchase_complete", function(self, items)
     --does the same _close_result_overlay (thus making that function redundant)
@@ -85,6 +92,10 @@ mod:hook_safe("CreditsGoodsVendorView", "_on_purchase_complete", function(self, 
         self:_remove_element("result_overlay")
     end
     Managers.event:trigger("event_vendor_view_purchased_item")
+    if total_stats() > STAT_THRESHOLD then 
+        mod:echo(mod.ERROR_MSG)
+        return
+    end
     
     for _, item_data in ipairs(items) do 
         local uuid = item_data.uuid
@@ -99,30 +110,26 @@ mod:hook_safe("CreditsGoodsVendorView", "_on_purchase_complete", function(self, 
 
             local comparing_stats = weapon_stats:get_comparing_stats() 
             local max_stats = ItemUtils.preview_stats_change(item, max_preview_expertise, comparing_stats)
+            --mod:echo("-----------------------------------------------------------------------------")
+            --mod:echo(item.__master_item.weapon_template)
 
             local isValidWeapon = true
             for i, stat in ipairs(comparing_stats) do
-                local user_value = 0
-                local max_stat = max_stats[stat.display_name]
-                if (i == 2) then
-                    user_value = mod["_stat_" .. 5]
-                elseif (i == 5) then
-                    user_value = mod["_stat_" .. 2]
-                else 
-                    user_value = mod["_stat_" .. i]
-                end
-
-                if max_stat then
-                    local max_value = max_stat.value or max_stat.fraction
-                    if max_value < user_value then
-                        isValidWeapon = false
-                        break
+                local user_stat = mod._user_stats[stat.display_name]
+                if user_stat then
+                    local purchased_max_value = max_stats[stat.display_name]
+                    if purchased_max_value then
+                        local max_value = purchased_max_value.value or purchased_max_value.fraction
+                        if max_value < user_stat.value then
+                            isValidWeapon = false
+                            break
+                        end
                     end
                 end
             end
             if isValidWeapon then
                 ItemUtils.set_item_id_as_favorite(itemID, true)
-                mod:notify("Weapon found with requested stat profile")
+                mod:notify("WEAPON FOUND WITH REQUESTED STAT PROFILE")
             end
         end
     end   
@@ -152,24 +159,84 @@ end)
 
 mod:hook_safe(Managers.event, "trigger", function(self, event_name, ...)
     if event_name == "event_vendor_view_purchased_item" then
+        --THIS SHOULDNT BE HERE -> set to true IF THE WEAPON IS TO BE FAVOURITED OTHERWISE FALSE
         mod._just_purchased = true
     end
 end)
 
---activates when switching between melee and ranged tabs
-mod:hook_safe("CreditsGoodsVendorView", "cb_switch_tab", function()
-    mod:notify("cb_switch_tab")
+mod:hook_safe("CreditsGoodsVendorView", "_preview_item", function(self, item)
+    mod._selected_weapon = item.weapon_template
+    local weapon_stats = WeaponTemplates[mod._selected_weapon]
+    _init()
+    local slider_val = 1
+    local count = 0
+    for _, stat_data in pairs(weapon_stats.base_stats) do
+        mod._user_stats[stat_data.display_name] = {slider = "slider_"..slider_val, value = 0}
+        slider_val = slider_val+1
+        count = count+1
+    end
+    --This needs to be there
+    for stat, data in pairs(mod._user_stats) do
+        local slider = self._widgets_by_name[data.slider]
+
+        if slider then
+            local content = slider.content
+            content.slider_value = data.value
+            content.value_text = mod:localize(stat) .. ":" .. tostring(data.value)
+        end
+    end
 end)
 
---activates on pressing an entry in brunts armoury
-mod:hook_safe("CreditsGoodsVendorView", "cb_on_grid_entry_left_pressed", function()
-    --mod:notify("cb_on_grid_entry_left_pressed")
+mod:hook_safe("CreditsGoodsVendorView", "update", function(self)
+    if next(mod._user_stats) == nil then return end 
+
+    for stat, data in pairs(mod._user_stats) do
+        local slider = self._widgets_by_name[data.slider]
+
+        if slider then
+            local content = slider.content
+            local raw_val = content.min + content.slider_value*(content.max - content.min)
+            local stepped_value = math.floor(raw_val+0.5)
+
+            if stepped_value ~= data.value then
+                content.value_text = mod:localize(stat) .. ": " .. tostring(stepped_value)
+                data.value = stepped_value
+            end
+        end
+    end
 end)
 
---activates on increasing weapon stats at hadron
-mod:hook_safe(CraftingUtil, "add_weapon_expertise", function()
-    mod:notify("add_weapon_expertise")
-end)
+local append_to_vendor_view_defs = function(defs)
+    if not defs then return end
+    defs.grid_settings = defs.grid_settings or {}
+    defs.scenegraph_definition = defs.scenegraph_definition or {}
+    defs.widget_definitions = defs.widget_definitions or {}
 
+    local x_offset = (defs.grid_settings.grid_spacing and defs.grid_settings.grid_spacing[1] * 2.5) or 25
+    local y_offset = (defs.grid_settings.title_height or 50) + 50
+
+    for i = 1, NUMBER_OF_STATS do
+        local key = "slider_"..i
+        defs.scenegraph_definition[key] = {
+            vertical_alignment = "left",
+            parent = "canvas",
+            horizontal_alignment = "center",
+            size = {510, 30},
+            position = {x_offset, y_offset, 3}
+        }
+        y_offset = y_offset+50
+
+        defs.widget_definitions[key] = UIWidget.create_definition(SliderPassTemplates.value_slider(520, 30, 400, true, false), key)
+
+        local widget_def = defs.widget_definitions[key]
+        if widget_def then
+            local content = widget_def.content
+            content.slider_value = 0
+            content.max = 80
+            content.min = 0
+        end
+    end
+end
+
+mod:hook_require(views.."credits_goods_vendor_view/credits_goods_vendor_view_definitions", append_to_vendor_view_defs)
 _init()
-stat_check(true)
--- BuyUntilStatDistribution_localization.lua
+++ BuyUntilStatDistribution_localization.lua
@@ -1,3 +1,6 @@
+local mod = get_mod("BuyUntilStatDistribution")
+local WeaponNames = mod:io_dofile("BuyUntilStatDistribution/weapon_names")
+
 return {
 	mod_name = {
 		en = "Buy Until Stat Distribution",
@@ -23,4 +26,34 @@ return {
 	stat_5 = {
 		en = "Weapon stat 5",
 	},
+	test_1 = {
+		en = "Test 1"
+	},
+	test_2 = {
+		en = "Test 2"
+	},
+	weapon_family = {
+		en = "Weapon Family"
+	},
+	loc_stats_display_ap_stat = {
+		en = "Penetration"
+	},
+	loc_stats_display_defense_stat = {
+		en = "Defences"
+	},
+	loc_stats_display_damage_stat = {
+		en = "Damage"
+	},
+	loc_stats_display_first_target_stat = {
+		en = "First Target"
+	},
+	loc_stats_display_mobility_stat = {
+		en = "Mobility"
+	},
+	loc_stats_display_finesse_stat = {
+		en = "Finesse"
+	},
+	loc_stats_display_crit_stat = {
+		en = "Critical Bonus"
+	}
 }
